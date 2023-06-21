e6fdbb062a284c379deea00f4888947c86645dd0
MaiiaHa <maiia.robeiko@ukr.net>
2023-06-21 14:45:46 +0300

    updates

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-import './js/mobile-menu';
-// import './js/header-scroll';
-import './js/modal';
-import './js/slider';
-import './js/btn-up';
+import './js/services/service-api';
+import './js/pagination';
+import './js/events/renderGalleryCard';
+
+import './js/modal/modal';
+import './js/modal/teamModal';
+
+import './js/loader';
+
+import './js/events/scrollUp';
+
+import './js/services/search-by-keyword';
+import './js/events/filter';
+import './js/events/themeSwitcher';
+import './js/events/preloader';
+
+import '../node_modules/basiclightbox/dist/basicLightbox.min.css';
